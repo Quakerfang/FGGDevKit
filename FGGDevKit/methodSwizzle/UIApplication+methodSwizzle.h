41d49286911ad2249dcbd92741ffdddc1fef3990
fgg <635926147@qq.com>
2016-05-17 09:50:17 +0800

    把动态库安装路径改为@executable_path，添加对UIApplication的启动函数拦截

---
+++ FGGDevKit/methodSwizzle/UIApplication+methodSwizzle.h
@@ -0,0 +1,13 @@
+//
+//  UIApplication+methodSwizzle.h
+//  FGGDevKit
+//
+//  Created by fgg on 16/5/17.
+//  Copyright © 2016年 fgg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIApplication (methodSwizzle)
+
+@end
---
+++ FGGDevKit/methodSwizzle/UIApplication+methodSwizzle.m
@@ -0,0 +1,13 @@
+//
+//  UIApplication+methodSwizzle.m
+//  FGGDevKit
+//
+//  Created by fgg on 16/5/17.
+//  Copyright © 2016年 fgg. All rights reserved.
+//
+
+#import "UIApplication+methodSwizzle.h"
+
+@implementation UIApplication (methodSwizzle)
+
+@end
